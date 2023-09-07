860d4d1e8e9fd4a50917e132c72084e8ad2a28d3
백재원 <bjw1622@gmail.com>
2023-09-07 14:49:21 +0900

    feat : guestbook02 finish

--- guestbook02/src/main/java/com/poscodx/guestbook/dao/GuestbookDao.java
+++ guestbook02/src/main/java/com/poscodx/guestbook/dao/GuestbookDao.java
@@ -136,8 +136,8 @@ public class GuestbookDao {
 		Connection conn = null;
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
-			String url = "jdbc:mariadb://192.168.0.177:3307/webdb?charset=utf8";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
+			String url = "jdbc:mariadb://192.168.64.2:3307/webdb?charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "qorwodnjs1!");
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
 		}
